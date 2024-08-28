1f3a42fc1849119ac34347ef88ff8b9de1baafc2
Ashleys Jane <153832650+22ashleyy@users.noreply.github.com>
2024-08-28 07:28:33 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = input("What is your age?")
+location = input("What location are you currently in?")
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
